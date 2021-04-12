a58f2ab9ce0fb68de3d815cfb9f0b7a72eb693a7
mohammedsobhee <mohammedsobhee@gmail.com>
2021-04-12 09:21:10 +0300

    ...

--- database/migrations/2020_04_12_021905_activity_log.php
+++ database/migrations/2020_04_12_021905_activity_log.php
@@ -14,6 +14,11 @@ class ActivityLog extends Migration
     public function up()
     {
         //
+        Schema::create('activity_log', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->longText('description')->unique()->nullable();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -24,5 +29,7 @@ class ActivityLog extends Migration
     public function down()
     {
         //
+        Schema::dropIfExists('activity_log');
+
     }
 }
