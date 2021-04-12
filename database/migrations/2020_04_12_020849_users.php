d3758dc2860394944ba6ee41d1ee1f3578970c90
mohammedsobhee <mohammedsobhee@gmail.com>
2021-04-12 09:11:31 +0300

    ...

--- database/migrations/2020_04_12_020849_users.php
+++ database/migrations/2020_04_12_020849_users.php
@@ -14,6 +14,10 @@ class Users extends Migration
     public function up()
     {
         //
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -24,5 +28,7 @@ class Users extends Migration
     public function down()
     {
         //
+        Schema::dropIfExists('users');
+
     }
 }
